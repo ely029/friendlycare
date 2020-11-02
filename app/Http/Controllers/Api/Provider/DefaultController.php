5125d16c9b038fd32b2f2031203294411bc18a4d
ely <eleazarembuscado1@gmail.com>
2020-11-02 18:02:10 +0800

    correct query

--- app/Http/Controllers/Api/Provider/DefaultController.php
+++ app/Http/Controllers/Api/Provider/DefaultController.php
@@ -550,7 +550,7 @@ class DefaultController extends Controller
         $clinic = DB::table('clinics')
             ->join('clinic_service', 'clinic_service.clinic_id', 'clinics.id')
             ->join('family_plan_type_subcategory', 'clinic_service.service_id', 'family_plan_type_subcategory.id')
-            ->select('clinics.clinic_name', 'clinics.photo_url', 'clinics.street_address', 'clinics.type', 'clinics.philhealth_acredited_1')
+            ->select('clinics.clinic_name', 'clinics.photo_url', 'clinics.street_address', 'clinics.type', 'clinics.philhealth_accredited_1')
             ->where('clinic_service.service_id', $id)
             ->get();
 
