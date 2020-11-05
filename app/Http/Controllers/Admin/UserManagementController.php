2956d2f6b962d5acc998c6187904bd1dd0ea00dc
ely <eleazarembuscado1@gmail.com>
2020-11-05 17:48:00 +0800

    fixes

--- app/Http/Controllers/Admin/UserManagementController.php
+++ app/Http/Controllers/Admin/UserManagementController.php
@@ -139,7 +139,7 @@ class UserManagementController extends Controller
             ->select('id', 'clinic_name')
             ->where('clinic_name', '<>', null)
             ->where('is_approve', '<>', 1)
-            ->where('user_id', null)
+            ->where('user_id', 0)
             ->get();
 
         return view('admin.userManagement.adminEditProfile', ['users' => $user, 'clinic' => $clinic]);
