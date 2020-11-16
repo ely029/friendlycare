373f7c6f52462bc3bf923d1f7d72c49edb1024b6
ely <eleazarembuscado1@gmail.com>
2020-11-16 12:39:27 +0800

    fies

--- app/Booking.php
+++ app/Booking.php
@@ -24,9 +24,9 @@ class Booking extends Model
     public function getBookingByPatient($patient_id)
     {
         return DB::table('booking')
-            ->join('clinic_service', 'clinic_service.service_id', 'booking.service_id')
-            ->join('service', 'service.id', 'clinic_service.service_id')
-            ->join('users', 'users.id', 'booking.patient_id')
+            ->leftJoin('clinic_service', 'clinic_service.service_id', 'booking.service_id')
+            ->leftJoin('service', 'service.id', 'clinic_service.service_id')
+            ->leftJoin('users', 'users.id', 'booking.patient_id')
             ->select('users.id as user_id', 'booking.id as booking_id', 'users.name', 'service.name as service_name', 'booking.time_slot', 'booking.status')
             ->where('booking.patient_id', $patient_id)
             ->where('booking.is_approved', 1)
