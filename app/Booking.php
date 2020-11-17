50e112ee8011acbec815baf77dcc192cf93f7c77
ely <eleazarembuscado1@gmail.com>
2020-11-17 12:00:08 +0800

    fixes

--- app/Booking.php
+++ app/Booking.php
@@ -43,7 +43,7 @@ class Booking extends Model
             ->select('users.id as user_id', 'booking.is_read', 'booking.id as booking_id', 'users.name', 'family_plan_type_subcategory.name as service_name', 'booking_time.time_slot', 'booking.status', 'booking.time_slot as date_booked')
             ->where('booking.clinic_id', $clinic_id)
             ->where('booking.is_booked', 1)
-            ->where('booking.status', null)
+            ->where('booking.status', '<>', null)
             ->whereBetween('booking.time_slot', [$date, $date])
             ->get();
     }
@@ -57,7 +57,7 @@ class Booking extends Model
             ->select('users.id as user_id', 'booking.is_read', 'booking.id as booking_id', 'users.name', 'family_plan_type_subcategory.name as service_name', 'booking_time.time_slot', 'booking.status', 'booking.time_slot as date_booked')
             ->where('booking.clinic_id', $clinic_id)
             ->where('booking.is_booked', 1)
-            ->where('booking.status', null)
+            ->where('booking.status', '<>', null)
             ->where('booking.time_slot', $date)
             ->get();
     }
@@ -71,7 +71,7 @@ class Booking extends Model
             ->select('users.id as user_id', 'booking.is_read', 'booking.id as booking_id', 'users.name', 'family_plan_type_subcategory.name as service_name', 'booking_time.time_slot', 'booking.status', 'booking.time_slot as date_booked')
             ->where('booking.clinic_id', $clinic_id)
             ->where('booking.is_booked', 1)
-            ->where('booking.status', null)
+            ->where('booking.status', '<>', null)
             ->where('booking.time_slot', $date)
             ->get();
     }
