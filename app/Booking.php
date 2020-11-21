76325cc19919c2e118cb4c6b082d3ec54af08884
ely <eleazarembuscado1@gmail.com>
2020-11-21 17:57:57 +0800

    fix

--- app/Booking.php
+++ app/Booking.php
@@ -145,6 +145,7 @@ class Booking extends Model
             ->select('users.id as user_id', 'booking.id as booking_id', 'booking.time_slot as date_booked', 'booking_time.time_slot', 'booking.status', 'clinics.clinic_name')
             ->where('booking.patient_id', $id)
             ->where('booking.status', '<>', 3)
+            ->where('booking_time.time_slot', '<>', null)
             ->get();
     }
 
