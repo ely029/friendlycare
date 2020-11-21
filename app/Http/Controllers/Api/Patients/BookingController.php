a547b93a13cdcc7cada29dd84252eb78d6545bc9
ely <eleazarembuscado1@gmail.com>
2020-11-22 00:30:20 +0800

    fix

--- app/Http/Controllers/Api/Patients/BookingController.php
+++ app/Http/Controllers/Api/Patients/BookingController.php
@@ -397,7 +397,7 @@ class BookingController extends Controller
         $clinic = Staffs::where('user_id', $id)->pluck('clinic_id');
         $booking = new Booking();
         $timenow = strtotime(date('Y-m-d H:i'));
-        DB::update('update booking set status = 5 where end_time <='.$timenow.'');
+        DB::update('update booking set status = 5 where new_request_end_time <='.$timenow.'');
         $endTime = DB::table('booking')->select('time_from', 'time_to')->where('clinic_id', $clinic[0])->get();
         $this->checkNoShow($clinic, $endTime);
         $details = $booking->getNewRequestBooking($clinic[0]);
