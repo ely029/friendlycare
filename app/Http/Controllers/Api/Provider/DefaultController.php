a4dd09b08261d87557d45461fc1fe041e3514ec3
ely <eleazarembuscado1@gmail.com>
2020-11-21 16:11:16 +0800

    fix

--- app/Http/Controllers/Api/Provider/DefaultController.php
+++ app/Http/Controllers/Api/Provider/DefaultController.php
@@ -571,11 +571,11 @@ class DefaultController extends Controller
         $data = DB::table('patient_time_slot')
             ->select('number_of_slots as number')
             ->where('clinic_id', $clinic[0])
-            ->get();
+            ->first();
 
         return response([
             'name' => 'GetTimeSlot',
-            'details' => $data,
+            'details' => $data->number_of_slots,
         ]);
     }
 
