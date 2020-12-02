f88157f1d7016a6120c2628d141211b1fd97571c
Xandy Juanitas <xjuanitas@gmail.com>
2020-12-02 21:58:41 +0800

    added front-end password pages

--- resources/views/password/index.blade.php
+++ resources/views/password/index.blade.php
@@ -1,7 +1,67 @@
 @extends('layouts.admin.app')
 
 @section('content')
-<div class="container">
+<!-- create password -->
+<div class="login">
+    <div class="login__container login__container--password">
+    <div class="login__top">
+        <div class="login__wrapper"><img class="login__image" src="img/logo.png" alt="Logo of e-Plano" /></div>
+        <div class="login__title">
+        <h2 class="login__text">e-Plano</h2>
+        <span class="login__span">
+            Family Planning Informational <br />
+            &amp; Booking App
+        </span>
+        </div>
+    </div>
+    <div class="login__content">
+        <div class="login__card">
+        <h2 class="section__heading">Welcome!</h2>
+        <p class="login__details">e-Plano has created an account for you. Please create a password to complete the process (Minimum of 8 characters).</p>
+        <form class="form form--login" action="">
+            <div class="form__content"><input class="form__input" id="js-password" type="password" placeholder="Enter password" /><label class="form__label">Enter password</label><i class="fa fa-eye-slash" id="js-eye"></i></div>
+            <div class="form__content"><input class="form__input" id="js-password" type="password" placeholder="Confirm password" /><label class="form__label">Confirm password</label><i class="fa fa-eye-slash" id="js-eye"></i></div>
+            <div class="form__button"><a class="button" href="">Create account</a></div>
+        </form>
+        </div>
+        <div class="login__card">
+        <h2 class="section__heading">Account details</h2>
+        <form class="form form--login" action="">
+            <div class="form__content form__content--reverse"><label class="form__label form__label--visible">Email</label><span class="form__text">johnsmith@gmail.com</span></div>
+            <div class="form__content form__content--reverse"><label class="form__label form__label--visible">Provider clinic</label><span class="form__text">Shaw Clinic</span></div>
+        </form>
+        </div>
+    </div>
+    </div>
+</div>
+
+<!-- reset password -->
+<div class="login login--success">
+    <div class="login__container login__container--password">
+    <div class="login__top">
+        <div class="login__wrapper"><img class="login__image" src="img/logo.png" alt="Logo of e-Plano" /></div>
+        <div class="login__title">
+        <h2 class="login__text">e-Plano</h2>
+        <span class="login__span">
+            Family Planning Informational <br />
+            &amp; Booking App
+        </span>
+        </div>
+    </div>
+    <div class="login__card">
+        <h2 class="section__heading">Password reset</h2>
+        <p class="login__details">Enter your new password (Minimum of 8 characters).</p>
+        <form class="form form--login" action="">
+        <div class="form__content"><input class="form__input" id="js-password" type="password" placeholder="Enter password" /><label class="form__label">Enter password</label><i class="fa fa-eye-slash" id="js-eye"></i></div>
+        <div class="form__content"><input class="form__input" id="js-password" type="password" placeholder="Confirm password" /><label class="form__label">Confirm password</label><i class="fa fa-eye-slash" id="js-eye"></i></div>
+        <div class="form__button"><a class="button" href="">Create account</a></div>
+        </form>
+    </div>
+    </div>
+</div>
+
+
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -45,5 +105,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
--- resources/views/password/thankyou.blade.php
+++ resources/views/password/thankyou.blade.php
@@ -1,1 +1,50 @@
-Your password are now updated <a href="{{ route('index') }}">Click Here</a>
+@extends('layouts.admin.app')
+
+@section('content')
+<!-- password creation success -->
+<div class="login login--success">
+    <div class="login__container login__container--password">
+    <div class="login__top">
+        <div class="login__wrapper"><img class="login__image" src="img/logo.png" alt="Logo of e-Plano" /></div>
+        <div class="login__title">
+        <h2 class="login__text">e-Plano</h2>
+        <span class="login__span">
+            Family Planning Informational <br />
+            &amp; Booking App
+        </span>
+        </div>
+    </div>
+    <div class="login__card">
+        <h2 class="section__heading">Success!</h2>
+        <p class="login__details">
+        You may now use your email and password to access and manage your clinic. <br />
+        <br />
+        Download the app to get started.
+        </p>
+    </div>
+    </div>
+</div>
+
+<!-- resetting password success -->
+
+<div class="login login--success">
+    <div class="login__container login__container--password">
+    <div class="login__top">
+        <div class="login__wrapper"><img class="login__image" src="img/logo.png" alt="Logo of e-Plano" /></div>
+        <div class="login__title">
+        <h2 class="login__text">e-Plano</h2>
+        <span class="login__span">
+            Family Planning Informational <br />
+            &amp; Booking App
+        </span>
+        </div>
+    </div>
+    <div class="login__card">
+        <h2 class="section__heading">Success!</h2>
+        <p class="login__details">Password has been reset.</p>
+    </div>
+    </div>
+</div>
+@endsection
+
+<!-- Your password are now updated <a href="{{ route('index') }}">Click Here</a> -->
