5acd6f58a5e2961412c470529ac0ef97085408a6
Xandy Juanitas <xjuanitas@gmail.com>
2020-12-03 17:58:51 +0800

    fixed styles

--- resources/views/admin/basicPages/informationPage.blade.php
+++ resources/views/admin/basicPages/informationPage.blade.php
@@ -31,7 +31,7 @@
 <div class="section">
 <div class="section__top">
     <h1 class="section__title">{{ $contentss->content_name }}</h1>
-    <div class="breadcrumbs"><a class="breadcrumbs__link" href="basic-pages.php">{{ $contentss->content_name }}</a><a class="breadcrumbs__link" href="consent-form.php">{{ $contentss->content_name }}</a><a class="breadcrumbs__link"></a></div>
+    <div class="breadcrumbs"><a class="breadcrumbs__link" href="basic-pages.php">Basic Pages</a><a class="breadcrumbs__link" href="consent-form.php">{{ $contentss->content_name }}</a><a class="breadcrumbs__link"></a></div>
 </div>
 <div class="section__container">
     <h2 class="section__heading">{{ $contentss->content_name }}</h2>
