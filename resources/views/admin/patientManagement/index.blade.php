132dfe59decfea0478a7b973ecd7926dd134a08d
ely <eleazarembuscado1@gmail.com>
2020-12-10 11:48:16 +0800

    patient management index page

---
+++ resources/views/admin/patientManagement/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin.dashboard')
+
+@section('title', 'Patient Management')
+@section('description', 'Dashboard')
+
+@section('content')
+<div class="wrapper">
+@include('includes.sidebar')
+</div>
+      <div class="section">
+        
+      </div>
+</div>
+@endsection
