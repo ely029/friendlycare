cf9d9cdc43fe36bd250d4590543b9836774d7075
ely <eleazarembuscado1@gmail.com>
2020-12-11 09:01:37 +0800

    fix

---
+++ resources/views/admin/survey/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin.dashboard')
+
+@section('title', 'Survey')
+@section('description', 'Dashboard')
+
+@section('content')
+<div class="wrapper">
+@include('includes.sidebar');
+</div>
+@endsection
