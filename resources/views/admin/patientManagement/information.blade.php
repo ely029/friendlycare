e9e4e884991c21ad9e12bc1390af566643516144
Xandy Juanitas <xjuanitas@gmail.com>
2021-01-05 15:53:50 +0800

    added view page of patient management PBI

--- resources/views/admin/patientManagement/information.blade.php
+++ resources/views/admin/patientManagement/information.blade.php
@@ -11,7 +11,7 @@
 <div class="section">
         <div class="section__top">
           <h1 class="section__title">Patient Smith</h1>
-          <div class="breadcrumbs"><a class="breadcrumbs__link" href="patient-management.html">Patient Management</a><a class="breadcrumbs__link" href="view-patient.html">Patient Smith</a><a class="breadcrumbs__link"></a></div>
+          <div class="breadcrumbs"><a class="breadcrumbs__link" href="patient-management.html">Patient Management</a><a class="breadcrumbs__link" >Patient Smith</a><a class="breadcrumbs__link"></a></div>
         </div>
         <div class="section__container">
           <form class="form form--viewPatient" action="">
