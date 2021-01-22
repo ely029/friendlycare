0ab6cbad24c24fd4095dd69e2d24809f02c53e6d
Xandy Juanitas <xjuanitas@gmail.com>
2021-01-22 15:15:49 +0800

    added privacy policy page

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin.dashboard')
+
+@section('content')
+
+<div class="privacy">
+  <div class="privacy__top">
+    <div class="privacy__wrapper"><img class="privacy__image" src="{URL::asset('img/logo.png')}}" alt="Logo of e-Plano" /></div>
+    <h2 class="privacy__top-title">e-Plano</h2>
+  </div>
+  <div class="privacy__container">
+    <h2 class="privacy__title">Privacy Policy</h2>
+    <p class="privacy__text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate magna eget mollis finibus. Nam sed convallis arcu, nec convallis purus. Morbi aliquet semper nisl, eu elementum ipsum vestibulum quis. Mauris auctor
+      neque nulla, at finibus elit laoreet et. Mauris convallis commodo elit, sit amet fringilla quam mattis eu. Donec rutrum ornare orci non aliquam. Sed venenatis libero quam, ut pretium velit aliquet id. Nulla facilisi. Etiam vel
+      eros vestibulum, varius sapien quis, convallis velit. Curabitur semper tellus vitae blandit feugiat. Sed sed augue lacinia, tempus nunc id, vehicula lorem. Aenean vel mauris hendrerit, consequat ante a, maximus ex. Interdum et
+      malesuada fames ac ante ipsum primis in faucibus. Curabitur gravida efficitur pulvinar. Integer viverra augue ut rutrum placerat.
+    </p>
+  </div>
+</div>
+
+@endsection
