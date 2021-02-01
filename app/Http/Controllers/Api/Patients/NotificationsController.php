3c88bbae4640a25c47b568d5e227d8fd832fb85d
ely <eleazarembuscado1@gmail.com>
2021-02-01 20:38:58 +0800

    fix

--- app/Http/Controllers/Api/Patients/NotificationsController.php
+++ app/Http/Controllers/Api/Patients/NotificationsController.php
@@ -181,7 +181,7 @@ class NotificationsController extends Controller
         return 0;
     }
 
-    public function bobo($id)
+    public function scheduledEvents($id)
     {
         $getBoolean = DB::table('events_notification')->select('date_time_string')->where('date_string', '<>', null)->orderBy('id', 'desc')->where('schedule', 0)->pluck('date_string');
         if (strtotime(Carbon::now()->toDateTimeString()) >= $getBoolean[0]) {
