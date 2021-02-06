b9616907fabdfbc64ac3997d012e2fd0b2c1da0e
ely <eleazarembuscado1@gmail.com>
2021-02-06 23:23:08 +0800

    fix search clinic with tagged method

--- app/Classes/TaggedMethodWithProvinceAndCity.php
+++ app/Classes/TaggedMethodWithProvinceAndCity.php
@@ -17,7 +17,7 @@ class TaggedMethodWithProvinceAndCity
                 ->where('clinic_service.service_id', $getMethod[0])
                 ->Where('clinics.province', $obj['province'][0])
                 ->Where('clinics.city', $obj['city'][0])
-                ->Where('clinics.paid_service', 0)
+                ->Where('clinics.paid_service', 1)
                 ->where('clinics.philhealth_accredited_1', 1)
                 ->where('clinics.user_id', 0)
                 ->get();
@@ -30,7 +30,7 @@ class TaggedMethodWithProvinceAndCity
                 ->where('clinic_service.service_id', $getMethod[0])
                 ->Where('clinics.province', $obj['province'][0])
                 ->Where('clinics.city', $obj['city'][0])
-                ->Where('clinics.paid_service', 1)
+                ->Where('clinics.paid_service', 0)
                 ->where('clinics.philhealth_accredited_1', 0)
                 ->where('clinics.user_id', 0)
                 ->get();
@@ -43,7 +43,7 @@ class TaggedMethodWithProvinceAndCity
                 ->where('clinic_service.service_id', $getMethod[0])
                 ->Where('clinics.province', $obj['province'][0])
                 ->Where('clinics.city', $obj['city'][0])
-                ->Where('clinics.paid_service', 1)
+                ->Where('clinics.paid_service', 0)
                 ->where('clinics.philhealth_accredited_1', 1)
                 ->where('clinics.user_id', 0)
                 ->get();
@@ -56,7 +56,7 @@ class TaggedMethodWithProvinceAndCity
                 ->where('clinic_service.service_id', $getMethod[0])
                 ->where('clinics.province', $obj['province'][0])
                 ->where('clinics.city', $obj['city'][0])
-                ->Where('clinics.paid_service', 0)
+                ->Where('clinics.paid_service', 1)
                 ->where('clinics.philhealth_accredited_1', 0)
                 ->where('clinics.user_id', 0)
                 ->get();
