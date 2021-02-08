141ee8a7277e58da2cd452f936a5830afdc2bcbb
ely <eleazarembuscado1@gmail.com>
2021-02-08 17:39:12 +0800

    fix

--- app/Classes/TaggedMethodWithoutProvinceAndCity.php
+++ app/Classes/TaggedMethodWithoutProvinceAndCity.php
@@ -8,23 +8,23 @@ use App\Clinics;
 
 class TaggedMethodWithoutProvinceAndCity
 {
-    public function index($obj, $method)
+    public function index($obj)
     {
         $clinics = new Clinics();
         if ($obj['province'][0] === '' && $obj['city'][0] === '' && $obj['philhealth_accredited'][0] === 1 && $obj['free_consultation'][0] === 1) {
-            return $clinics->getTaggedMethodFirstScenario($method);
+            return $clinics->getTaggedMethodFirstScenario();
         }
 
         if ($obj['province'][0] === '' && $obj['city'][0] === '' && $obj['philhealth_accredited'][0] === 0 && $obj['free_consultation'][0] === 0) {
-            return $clinics->getTaggedMethodSecondScenario($method);
+            return $clinics->getTaggedMethodSecondScenario();
         }
 
         if ($obj['province'][0] === '' && $obj['city'][0] === '' && $obj['philhealth_accredited'][0] === 1 && $obj['free_consultation'][0] === 0) {
-            return $clinics->getTaggedMethodThirdScenario($method);
+            return $clinics->getTaggedMethodThirdScenario();
         }
 
         if ($obj['province'][0] === '' && $obj['city'][0] === '' && $obj['philhealth_accredited'][0] === 0 && $obj['free_consultation'][0] === 1) {
-            return $clinics->getTaggedMethodFourthScenario($method);
+            return $clinics->getTaggedMethodFourthScenario();
         }
     }
 }
--- app/Http/Controllers/Api/Patients/BookingController.php
+++ app/Http/Controllers/Api/Patients/BookingController.php
@@ -91,14 +91,14 @@ class BookingController extends Controller
     {
         $booking = new Booking();
         $obj = json_decode($request->getContent(), true);
-        $method = $booking->getMethod($id);
+        $booking->getMethod($id);
 
         if ($obj['province'][0] === '' && $obj['city'][0] === '') {
             $class = new TaggedMethodWithoutProvinceAndCity();
-            $clinic = $class->index($obj, $method);
+            $clinic = $class->index($obj);
         } else {
             $class = new TaggedMethodWithProvinceAndCity();
-            $clinic = $class->index($obj, $method);
+            $clinic = $class->index($obj);
         }
 
         return response([
