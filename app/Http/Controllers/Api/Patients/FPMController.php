bcc36d4cbd3650bff1914f6c89cffc0dbe7d7c49
ely <eleazarembuscado1@gmail.com>
2021-02-09 10:42:47 +0800

    fix

--- app/Http/Controllers/Api/Patients/FPMController.php
+++ app/Http/Controllers/Api/Patients/FPMController.php
@@ -16,7 +16,7 @@ class FPMController extends Controller
     {
         $fpmType = new FpmTypeService();
         if ($pageid === '1') {
-            $data = DB::table('patients')->select('fpm_user_type')->where('user_id', $id)->pluck('fpm_user_type');
+            $data = DB::table('patients')->select('fpm_plan_reasons')->where('user_id', $id)->pluck('fpm_user_type');
             if ($data[0] === null) {
                 return response([
                     'name' => 'fpmUserType',
