94f01c77814f8bf042ce02d58b656947c783c817
ely <eleazarembuscado1@gmail.com>
2021-02-09 09:43:07 +0800

    fix

--- app/Http/Controllers/Api/Patients/DefaultController.php
+++ app/Http/Controllers/Api/Patients/DefaultController.php
@@ -113,9 +113,7 @@ class DefaultController extends Controller
 
         $request['user_id'] = $user->id;
         $request['patient_id'] = $user->id;
-        Patients::where('user_id', $request['user_id'])->update([
-            'fpm_user_type' => 1,
-        ]);
+        DB::statement('update patients set fpm_user_type = 1 where user_id = ?', [$request['user_id']]);
 
         Patients::create($request);
         Spouses::create($request);
