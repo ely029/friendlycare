eb64b7a8a1bc76840a0470248fabfaf313e9b6d7
ely <eleazarembuscado1@gmail.com>
2021-02-24 10:19:46 +0800

    fix

--- app/User.php
+++ app/User.php
@@ -194,7 +194,7 @@ class User extends Authenticatable
     public function patientsLessNineteen($request)
     {
         return DB::table('users')->leftJoin('patients', 'users.id', 'patients.user_id')
-            ->select('users.id', 'users.name', 'users.email', 'users.age', 'patients.province', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at', 'family_plan_type_subcategory.name'))
+            ->select('users.id', 'users.name', 'users.email', 'users.age', 'patients.province', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), 'family_plan_type_subcategory.name')
             ->whereBetween('users.created_at', [$request['date-from'], $request['date-to']])
             ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
             ->leftJoin('family_plan_type_subcategory', 'family_plan_type_subcategory.id', 'fpm_type_service.service_id')
@@ -205,7 +205,7 @@ class User extends Authenticatable
     public function patientsMoreThanTwenty($request)
     {
         return DB::table('users')->leftJoin('patients', 'users.id', 'patients.user_id')
-            ->select('users.id', 'users.name', 'users.email', 'users.age', 'patients.province', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at', 'family_plan_type_subcategory.name'))
+            ->select('users.id', 'users.name', 'users.email', 'users.age', 'patients.province', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), 'family_plan_type_subcategory.name')
             ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
             ->leftJoin('family_plan_type_subcategory', 'family_plan_type_subcategory.id', 'fpm_type_service.service_id')
             ->whereBetween('users.created_at', [$request['date-from'], $request['date-to']])
