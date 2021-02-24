91bcffe9bc071ce21dd8fa6a0241117c569977c3
ely <eleazarembuscado1@gmail.com>
2021-02-24 10:51:48 +0800

    fix

--- app/User.php
+++ app/User.php
@@ -216,11 +216,20 @@ class User extends Authenticatable
     public function getPatientManagementDetails()
     {
         return DB::table('users')
-            ->leftjoin('patients', 'patients.user_id', 'users.id')
-            ->select('users.id', 'users.name', 'patients.province', 'users.email', 'users.age', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), 'family_plan_type_subcategory.name as service_name')
+            ->leftJoin('patients', 'patients.user_id', 'users.id')
+            ->distinct('users.name')
             ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
             ->leftJoin('family_plan_type_subcategory', 'family_plan_type_subcategory.id', 'fpm_type_service.service_id')
-            ->where('role_id', 3)
+            ->select('users.name', 'users.age', 'patients.province', 'patients.municipality', 'family_plan_type_subcategory.name as service_name', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), DB::raw('DATE_FORMAT(fpm_type_service.updated_at, "%m/%d/%Y") as date_last_update'), DB::raw('IF(fpm_type_service.service_id is null, "No", "Yes") as fpm_type_service'), DB::raw('
+            (
+                CASE 
+                WHEN patients.fpm_user_type = 1 THEN "New Acceptor"
+                WHEN patients.fpm_user_type = 2 THEN "Changing Methods"
+                WHEN patients.fpm_user_type = 3 THEN "Curent User"
+                WHEN patients.fpm_user_type = 4 THEN "Restart"
+                END
+            )AS fpm_2'), 'family_plan_type_subcategory.name as fpm_1')
+            ->where('users.role_id', 3)
             ->get();
     }
 }
