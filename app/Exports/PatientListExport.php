cc1eee45e1a68b04e56591694c4f68958fb44be5
ely <eleazarembuscado1@gmail.com>
2021-02-24 13:51:24 +0800

    fix

--- app/Exports/PatientListExport.php
+++ app/Exports/PatientListExport.php
@@ -46,7 +46,7 @@ class PatientListExport implements FromCollection, WithHeadings
                 ->where('users.age', '<=', 19)
                 ->get();
         }
-        if ($this->dateFrom === '0' && $this->dateTo === '0' && $this->age === '0') {
+        if ($this->dateFrom === '0' && $this->dateTo === '0' && $this->age === '2') {
             return DB::table('users')
                 ->leftJoin('patients', 'patients.user_id', 'users.id')
                 ->distinct('users.name')
@@ -84,6 +84,25 @@ class PatientListExport implements FromCollection, WithHeadings
                 ->whereBetween('users.created_at', [$this->dateFrom, $this->dateTo])
                 ->get();
         }
+        if ($this->age === '2' && $this->dateFrom !== '0' && $this->dateTo !== '0') {
+            return DB::table('users')
+                ->leftJoin('patients', 'patients.user_id', 'users.id')
+                ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
+                ->leftJoin('family_plan_type_subcategory', 'family_plan_type_subcategory.id', 'fpm_type_service.service_id')
+                ->select('users.name', 'users.age', 'patients.province', 'patients.municipality', 'family_plan_type_subcategory.name as service_name', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), DB::raw('DATE_FORMAT(fpm_type_service.updated_at, "%m/%d/%Y") as date_last_update'), DB::raw('IF(fpm_type_service.service_id is null, "No", "Yes") as fpm_type_service'), DB::raw('
+                    (
+                        CASE 
+                        WHEN patients.fpm_user_type = 1 THEN "New Acceptor"
+                        WHEN patients.fpm_user_type = 2 THEN "Changing Methods"
+                        WHEN patients.fpm_user_type = 3 THEN "Curent User"
+                        WHEN patients.fpm_user_type = 4 THEN "Restart"
+                        END
+                    )AS fpm_2'), 'family_plan_type_subcategory.name as fpm_1')
+                ->where('users.role_id', 3)
+                ->where('users.age', '>=', 20)
+                ->whereBetween('users.created_at', [$this->dateFrom, $this->dateTo])
+                ->get();
+        }
         return DB::table('users')
             ->leftJoin('patients', 'patients.user_id', 'users.id')
             ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
@@ -98,8 +117,6 @@ class PatientListExport implements FromCollection, WithHeadings
                         END
                     )AS fpm_2'), 'family_plan_type_subcategory.name as fpm_1')
             ->where('users.role_id', 3)
-            ->where('users.age', '>=', 20)
-            ->whereBetween('users.created_at', [$this->dateFrom, $this->dateTo])
             ->get();
     }
 }
