9b2e85dad22b89b5ee181c19cf6d0d3203acd714
ely <eleazarembuscado1@gmail.com>
2021-02-24 11:43:27 +0800

    fix

--- app/Exports/PatientListExport.php
+++ app/Exports/PatientListExport.php
@@ -65,6 +65,24 @@ class PatientListExport implements FromCollection, WithHeadings
                 ->where('users.age', '>=', 20)
                 ->get();
         }
+        if ($this->dateFrom === '0' && $this->dateTo === '0' && $this->age === null) {
+            return DB::table('users')
+                ->leftJoin('patients', 'patients.user_id', 'users.id')
+                ->distinct('users.name')
+                ->leftJoin('fpm_type_service', 'fpm_type_service.patient_id', 'users.id')
+                ->leftJoin('family_plan_type_subcategory', 'family_plan_type_subcategory.id', 'fpm_type_service.service_id')
+                ->select('users.name', 'users.age', 'patients.province', 'patients.municipality', 'family_plan_type_subcategory.name as service_name', DB::raw('DATE_FORMAT(users.created_at, "%m/%d/%Y") as registered_at'), DB::raw('DATE_FORMAT(fpm_type_service.updated_at, "%m/%d/%Y") as date_last_update'), DB::raw('IF(fpm_type_service.service_id is null, "No", "Yes") as fpm_type_service'), DB::raw('
+                    (
+                        CASE 
+                        WHEN patients.fpm_user_type = 1 THEN "New Acceptor"
+                        WHEN patients.fpm_user_type = 2 THEN "Changing Methods"
+                        WHEN patients.fpm_user_type = 3 THEN "Curent User"
+                        WHEN patients.fpm_user_type = 4 THEN "Restart"
+                        END
+                    )AS fpm_2'), 'family_plan_type_subcategory.name as fpm_1')
+                ->where('users.role_id', 3)
+                ->get();
+        }
         if ($this->age === '1') {
             return DB::table('users')
                 ->leftJoin('patients', 'patients.user_id', 'users.id')
