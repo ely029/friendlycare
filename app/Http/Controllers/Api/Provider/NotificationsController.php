5b91ce9ef67684abf43ddb5cbb7b68ff0861770b
ely <eleazarembuscado1@gmail.com>
2021-03-03 06:49:39 +0800

    fix provider notification tab

--- app/Http/Controllers/Api/Provider/NotificationsController.php
+++ app/Http/Controllers/Api/Provider/NotificationsController.php
@@ -21,6 +21,7 @@ class NotificationsController extends Controller
             ->where('clinic_id', $getClinicId[0]);
         $upcoming = DB::table('provider_notifications')
             ->select('id', 'title', 'type', 'status', 'is_read')
+            ->where('clinic_id', $getClinicId[0])
             ->whereRaw('DATEDIFF(date_booked, CURDATE()) = 1');
         $details = $notifications->union($upcoming)->get();
         return response([
