40b11bdcbc486007f14d2ce9932d6aa71833a1d5
ely <eleazarembuscado1@gmail.com>
2021-03-02 09:37:08 +0800

    put status in filtering

--- app/Http/Controllers/Api/Patients/NotificationsController.php
+++ app/Http/Controllers/Api/Patients/NotificationsController.php
@@ -196,7 +196,7 @@ class NotificationsController extends Controller
 
     public function bookingTommorow($id)
     {
-        $checkDisplay = DB::table('booking')->select('id')->where('book_tommorow_display', 0)->where('patient_id', $id)->count();
+        $checkDisplay = DB::table('booking')->select('id')->where('book_tommorow_display', 0)->where('status', 1)->where('patient_id', $id)->count();
         $getDate = DB::table('booking')->select('time_slot')->where('book_tommorow_display', 0)->limit(1)->orderBy('id', 'desc')->where('patient_id', $id)->pluck('time_slot');
         $checkDate = Carbon::parse(date('Y-m-d'))->diffInDays($getDate[0] ?? '0000-00-00');
         $pushNotifications = new PushNotifications();
