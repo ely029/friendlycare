309630b966f0329ef16b84f90c3dd8c4f738036d
ely <eleazarembuscado1@gmail.com>
2021-03-10 17:32:24 +0800

    fix

--- app/Http/Controllers/Api/Patients/BookingController.php
+++ app/Http/Controllers/Api/Patients/BookingController.php
@@ -539,19 +539,10 @@ class BookingController extends Controller
         if ($getSlot[0] <= $checkBooking) {
             return response()->json('The time you choose are already full. please choose another time.', 422);
         }
-        $this->updateBooking($obj, $id);
         $this->createBookingTime($id, $obj);
         return response([
             'response' => 'Booking Created Succesfully',
         ], 200);
-    }
-
-    private function updateBooking($obj, $id)
-    {
-        if ($obj['referal'][0] !== '') {
-            DB::update('update booking set time_slot = ?, status = ?, referal = ? where patient_id = ? order by id desc limit 1', [$obj['date'][0], 6, $obj['referal'][0], $id]);
-        }
-        DB::update('update booking set time_slot = ?, status = ? where patient_id = ? order by id desc limit 1', [$obj['date'][0], 6, $id]);
     }
 
     private function createBookingTime($id, $obj)
