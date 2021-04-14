bf62bd8bd8ff3eb59347a65046858fbaac007c1e
ely <eleazarembuscado1@gmail.com>
2021-04-14 22:56:36 +0800

    fix

--- app/Http/Controllers/Api/Patients/NotificationsController.php
+++ app/Http/Controllers/Api/Patients/NotificationsController.php
@@ -108,8 +108,8 @@ class NotificationsController extends Controller
             'display_type' => 'Notifications',
             'title' => 'Booking Cancelled',
             'status' => 3,
-            'clinic_name' => $getClinicName[0],
-            'date_booked' => $getTime[0],
+            'clinic_name_1' => $getClinicName[0],
+            'appointment_date_1' => $getTime[0],
         ]);
         Mail::send('email.patient.cancellation-booking', ['name' => $getPatientName[0], 'date' => $getTime[0]], function ($mail) use ($getClinicEmail) {
             $mail->from('notifications@friendlycare.com');
